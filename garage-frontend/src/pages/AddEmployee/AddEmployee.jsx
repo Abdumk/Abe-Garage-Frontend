1e6d425279333eb87291737bb062caccc9de0bb0
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-02-14 22:20:18 +0300

    Addemploye  handleSubmit fun

--- garage-frontend/src/pages/AddEmployee/AddEmployee.jsx
+++ garage-frontend/src/pages/AddEmployee/AddEmployee.jsx
@@ -7,6 +7,32 @@ function AddEmployee(props) {
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
 
+     // Write a function to handle the form submission
+  function handleSubmit(event) {
+    // Prevent the default behaviour of the form submission
+    event.preventDefault();
+    // Prepare the data to be sent to the server  
+    const data = {
+      first_name: firstName,
+      last_name: lastName,
+      email: emailAddress,
+      password: password
+    };
+    // Send the data to the server  
+    const apiUrl = "http://localhost:4000/add-employee";
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+    const response = fetch(apiUrl, requestOptions);
+    response.then(res => res.json())
+      .then(res => {
+        console.log(res);
+      });
+
+
+  }
   return (
     <div>
 
