ef6b75193308bcc330986afdab26e588bcc74e0b
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-02-14 22:11:50 +0300

    create components & start addemployee state and function

---
+++ garage-frontend/src/pages/AddEmployee/AddEmployee.jsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+function AddEmployee(props) {
+    // Declare state variables for each of the form fields  
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default AddEmployee
