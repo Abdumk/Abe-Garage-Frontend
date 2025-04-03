649abb57f5c276ebda2b1c074491b34b54122d58
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-04-03 23:09:53 +0300

    create mandatori components ,pages &services mainly login,addemploye,home

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,22 @@ import "./assets/styles/custom.css";
 import Header from '../src/markup/components/Header/Header';
 // Import the Footer component
 import Footer from '../src/markup/components/Footer/Footer';
-
+// import the Login component
+import Login from '../src/markup/components/LoginForm/LoginForm';
+// import the Home component
+import Home from '../src/markup/pages/Home';
+// import the AddEmployee component
+import AddEmployee from '../src/markup/pages/admin/AddEmployee';
 
 function App() {
   return (
     <>
       <Header />
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/add-employee" element={<AddEmployee />} />
+      </Routes>
       <Footer />
     </>
   );
